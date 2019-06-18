aa89412991572ee548f7afb250c73e5476401b4a
Antonio Martínez <joseamb95@gmail.com>
2019-06-18 16:38:31 -0500

    Custom Elements

--- codewars/rgb.js
+++ codewars/rgb.js
@@ -1,12 +1,14 @@
 const rgb = (r,g,b) => `${decHex(r)}${decHex(g)}${decHex(b)}`;
 const decHex = (str) => {
+ console.log(str);
  const hex = ['0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'];
  let dec = '';
  while(str > 0) {
+ 	console.log("dec", dec);
    dec = dec.concat(hex[Number(str)%16]);
    str = Math.trunc(Number(str)/16);
  }
  return dec.split("").reverse().join("");
 };
 
-console.log(rgb(0,0,0));
+console.log(rgb(165,165,165));
